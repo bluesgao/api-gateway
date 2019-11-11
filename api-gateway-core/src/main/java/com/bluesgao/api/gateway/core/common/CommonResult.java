fd2110f1806967624e91f8f700a9a151828dc9f9
gaoxin11 <gaoxin11@jd.com>
2019-11-11 21:24:25 +0800

    auth

--- api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/common/CommonResult.java
+++ api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/common/CommonResult.java
@@ -1,10 +1,14 @@
 package com.bluesgao.api.gateway.core.common;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommonResult<V> implements Serializable {
     private int code;
     private String message;
