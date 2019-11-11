63f16f627a563edd1c1c632330a27345fe938af4
gaoxin11 <gaoxin11@jd.com>
2019-11-11 18:16:43 +0800

    http parse

--- api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/handler/inbound/AuthenticationHandler.java
+++ api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/handler/inbound/AuthenticationHandler.java
@@ -1,62 +1,21 @@
 package com.bluesgao.api.gateway.core.handler.inbound;
 
+import com.alibaba.fastjson.JSON;
 import com.bluesgao.api.gateway.core.protocol.http.RequestParser;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.*;
-import io.netty.handler.codec.http.multipart.Attribute;
-import io.netty.handler.codec.http.multipart.DefaultHttpDataFactory;
-import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
-import io.netty.handler.codec.http.multipart.InterfaceHttpData;
+import io.netty.handler.codec.http.FullHttpRequest;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
 import java.util.Map;
-
-import static io.netty.handler.codec.http.HttpMethod.GET;
-import static io.netty.handler.codec.http.HttpMethod.POST;
 
 @Slf4j
 public class AuthenticationHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpRequest fullHttpRequest) throws Exception {
         log.info("AuthenticationHandler channelRead0");
-            HttpHeaders headers = fullHttpRequest.headers();
-            log.info("headers:{}", headers);
-            HttpMethod method = fullHttpRequest.method();
-            log.info("method:{}", method);
-
-            handleHttpRequest(channelHandlerContext,fullHttpRequest);
-
-            RequestParser requestParser = new RequestParser(fullHttpRequest);
-            Map<String, String> requestMap = requestParser.parse();
-            log.info("requestMap:{}", requestMap);
-
-    }
-
-    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) throws Exception {
-        if (req.method() == GET) { // 处理get请求
-
-            QueryStringDecoder decoder = new QueryStringDecoder(req.uri());
-            Map<String, List<String>> parms = decoder.parameters();
-            List<String> q = parms.get("data"); // 读取从客户端传过来的参数
-            String question = q.get(0);
-            if (question != null && !question.equals("")) {
-                System.out.println("r :" + question);
-            }
-        }else if (req.method() == POST) { // 处理POST请求
-            HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(
-                    new DefaultHttpDataFactory(false), req);
-            InterfaceHttpData postData = decoder.getBodyHttpData("username");
-            // 读取从客户端传过来的参数
-            String question = "";
-            if (postData!=null && postData.getHttpDataType() == InterfaceHttpData.HttpDataType.Attribute) {
-                Attribute attribute = (Attribute) postData;
-                question = attribute.getValue();
-                System.out.println("q:" + question);
-            }
-            return;
-        }
+        RequestParser requestParser = new RequestParser(fullHttpRequest);
+        Map<String, String> requestMap = requestParser.parse();
+        log.info("requestMap:{}", JSON.toJSONString(requestMap));
     }
 }
--- api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/protocol/http/RequestParser.java
+++ api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/protocol/http/RequestParser.java
@@ -1,11 +1,13 @@
 package com.bluesgao.api.gateway.core.protocol.http;
 
+import com.alibaba.fastjson.JSONObject;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.QueryStringDecoder;
 import io.netty.handler.codec.http.multipart.Attribute;
 import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
 import io.netty.handler.codec.http.multipart.InterfaceHttpData;
+import io.netty.util.CharsetUtil;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -14,55 +16,50 @@ import java.util.Map;
 
 /**
  * HTTP请求参数解析器, 支持GET, POST
- * Created by whf on 12/23/15.
  */
 public class RequestParser {
-    private FullHttpRequest fullReq;
+    private FullHttpRequest request;
 
     /**
      * 构造一个解析器
+     *
      * @param req
      */
     public RequestParser(FullHttpRequest req) {
-        this.fullReq = req;
+        this.request = req;
     }
 
     /**
      * 解析请求参数
-     * @return 包含所有请求参数的键值对, 如果没有参数, 则返回空Map
      *
+     * @return 包含所有请求参数的键值对, 如果没有参数, 则返回空Map
      * @throws IOException
      */
-    public Map<String, String> parse() throws  Exception {
-        HttpMethod method = fullReq.method();
-
+    public Map<String, String> parse() throws Exception {
+        HttpMethod method = request.method();
         Map<String, String> parmMap = new HashMap<>();
-
-        if (HttpMethod.GET == method) {
-            // 是GET请求
-            QueryStringDecoder decoder = new QueryStringDecoder(fullReq.uri());
-            decoder.parameters().entrySet().forEach( entry -> {
+        if (HttpMethod.GET == method) {// 是GET请求
+            QueryStringDecoder decoder = new QueryStringDecoder(request.uri());//获取get查询参数
+            decoder.parameters().entrySet().forEach(entry -> {
                 // entry.getValue()是一个List, 只取第一个元素
                 parmMap.put(entry.getKey(), entry.getValue().get(0));
             });
-        } else if (HttpMethod.POST == method) {
-            // 是POST请求
-            HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(fullReq);
-            decoder.offer(fullReq);
-
+        } else if (HttpMethod.POST == method) {// 是POST请求
+            HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(request);
+            if (request.content().isReadable()) {
+                String dataJson = request.content().toString(CharsetUtil.UTF_8);
+                parmMap.putAll(JSONObject.parseObject(dataJson, Map.class));
+            }
+            decoder.offer(request);//form表单
             List<InterfaceHttpData> parmList = decoder.getBodyHttpDatas();
-
             for (InterfaceHttpData parm : parmList) {
-
                 Attribute data = (Attribute) parm;
                 parmMap.put(data.getName(), data.getValue());
             }
-
         } else {
             // 不支持其它方法
             throw new Exception("HTTP请求只支持post和get方法，其它方法不支持");
         }
-
         return parmMap;
     }
 }
