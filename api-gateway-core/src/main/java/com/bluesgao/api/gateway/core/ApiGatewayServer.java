d6e73dd69d1fd6926cd2e88dd045e3ccef1d73d1
bluesgao <bluesgao@126.com>
2019-11-11 17:55:46 +0800

    http body

--- api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/ApiGatewayServer.java
+++ api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/ApiGatewayServer.java
@@ -7,8 +7,7 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
@@ -32,12 +31,13 @@ public class ApiGatewayServer {
                         protected void initChannel(SocketChannel ch) throws Exception {
                             log.info("initChannel...");
                             // server端发送的是httpResponse，所以要使用HttpResponseEncoder进行编码
-                            ch.pipeline().addLast(new HttpResponseEncoder());
+                            //ch.pipeline().addLast(new HttpResponseEncoder());
                             // server端接收到的是httpRequest，所以要使用HttpRequestDecoder进行解码
-                            ch.pipeline().addLast(new HttpRequestDecoder());
-                            ch.pipeline().addLast(new AuthenticationHandler());
-                            //增加自定义实现的Handler
+                            //ch.pipeline().addLast(new HttpRequestDecoder());
                             ch.pipeline().addLast(new HttpServerCodec());
+                            ch.pipeline().addLast( new HttpObjectAggregator(65536));
+                            //ch.pipeline().addLast(new ChunkedWriteHandler());
+                            ch.pipeline().addLast(new AuthenticationHandler());
                         }
                     });
 
--- api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/handler/inbound/AuthenticationHandler.java
+++ api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/handler/inbound/AuthenticationHandler.java
@@ -3,6 +3,7 @@ package com.bluesgao.api.gateway.core.handler.inbound;
 import com.bluesgao.api.gateway.core.protocol.http.RequestParser;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.codec.http.multipart.Attribute;
 import io.netty.handler.codec.http.multipart.DefaultHttpDataFactory;
@@ -17,32 +18,29 @@ import static io.netty.handler.codec.http.HttpMethod.GET;
 import static io.netty.handler.codec.http.HttpMethod.POST;
 
 @Slf4j
-public class AuthenticationHandler extends ChannelInboundHandlerAdapter {
+public class AuthenticationHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        log.info("AuthenticationHandler channelRead");
-        super.channelRead(ctx, msg);
-        if (msg instanceof HttpRequest) {
-            HttpRequest request = (HttpRequest) msg;
-            HttpHeaders headers = request.headers();
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpRequest fullHttpRequest) throws Exception {
+        log.info("AuthenticationHandler channelRead0");
+            HttpHeaders headers = fullHttpRequest.headers();
             log.info("headers:{}", headers);
-            HttpMethod method = request.method();
+            HttpMethod method = fullHttpRequest.method();
             log.info("method:{}", method);
 
-            handleHttpRequest(ctx,request);
+            handleHttpRequest(channelHandlerContext,fullHttpRequest);
 
-            RequestParser requestParser = new RequestParser((FullHttpRequest) msg);
+            RequestParser requestParser = new RequestParser(fullHttpRequest);
             Map<String, String> requestMap = requestParser.parse();
             log.info("requestMap:{}", requestMap);
-        }
+
     }
 
-    private void handleHttpRequest(ChannelHandlerContext ctx, HttpRequest req) throws Exception {
+    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) throws Exception {
         if (req.method() == GET) { // 处理get请求
 
-            QueryStringDecoder decoder = new QueryStringDecoder(req.getUri());
-            Map<String, List<String>> parame = decoder.parameters();
-            List<String> q = parame.get("q"); // 读取从客户端传过来的参数
+            QueryStringDecoder decoder = new QueryStringDecoder(req.uri());
+            Map<String, List<String>> parms = decoder.parameters();
+            List<String> q = parms.get("data"); // 读取从客户端传过来的参数
             String question = q.get(0);
             if (question != null && !question.equals("")) {
                 System.out.println("r :" + question);
@@ -53,24 +51,12 @@ public class AuthenticationHandler extends ChannelInboundHandlerAdapter {
             InterfaceHttpData postData = decoder.getBodyHttpData("username");
             // 读取从客户端传过来的参数
             String question = "";
-            if (postData.getHttpDataType() == InterfaceHttpData.HttpDataType.Attribute) {
+            if (postData!=null && postData.getHttpDataType() == InterfaceHttpData.HttpDataType.Attribute) {
                 Attribute attribute = (Attribute) postData;
                 question = attribute.getValue();
                 System.out.println("q:" + question);
             }
             return;
         }
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        log.info("AuthenticationHandler exceptionCaught");
-        super.exceptionCaught(ctx, cause);
-    }
-
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        log.info("AuthenticationHandler exceptionCaught");
-        super.channelReadComplete(ctx);
     }
 }
