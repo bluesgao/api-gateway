8b21b892c5060ce70c97c9a3e8c0cea748005bcf
gaoxin11 <gaoxin11@jd.com>
2019-11-13 16:08:45 +0800

    通用返回码

--- api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/common/CommonResult.java
+++ api-gateway-core/src/main/java/com/bluesgao/api/gateway/core/common/CommonResult.java
@@ -1,16 +1,95 @@
 package com.bluesgao.api.gateway.core.common;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serializable;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class CommonResult<V> implements Serializable {
-    private String code;
-    private String message;
-    private V result;
+public class CommonResult<T> implements Serializable {
+
+    private static final long serialVersionUID = -4606232502008831307L;
+    /**
+     * 响应码
+     */
+    private String resultCode;
+    /**
+     * 响应信息
+     */
+    private String resultMsg;
+    /**
+     * 返回值
+     */
+    private T value;
+
+    public static Builder custom() {
+        return new Builder();
+    }
+
+    public static class Builder<T> {
+
+        private String resultCode;
+
+        private String resultMsg;
+
+        private T value;
+
+
+        public Builder<T> code(String resultCode) {
+            this.resultCode = resultCode;
+            return this;
+        }
+
+        public Builder<T> info(String resultMsg) {
+            this.resultMsg = resultMsg;
+            return this;
+        }
+
+        public Builder<T> value(T value) {
+            this.value = value;
+            return this;
+        }
+
+        public CommonResult<T> build() {
+            return new CommonResult(this);
+        }
+
+    }
+
+    public CommonResult() {
+
+    }
+
+    private CommonResult(Builder builder) {
+        resultCode = builder.resultCode;
+        resultMsg = builder.resultMsg;
+        value = (T) builder.value;
+    }
+
+    public CommonResult<T> buildSuccess(T t) {
+        this.resultCode = ResultCodeEnum.SUCCESS.getCode();
+        this.resultMsg = ResultCodeEnum.SUCCESS.getMessage();
+        this.value = t;
+        return this;
+    }
+
+    public CommonResult<T> buildError(ResultCodeEnum resultCodeEnum) {
+        this.resultCode = resultCodeEnum.getCode();
+        this.resultMsg = resultCodeEnum.getMessage();
+        return this;
+    }
+
+    public CommonResult<T> buildError(String resultCode, String message) {
+        this.resultCode = resultCode;
+        this.resultMsg = message;
+        return this;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public String getResultMsg() {
+        return resultMsg;
+    }
+
+    public T getValue() {
+        return value;
+    }
 }
